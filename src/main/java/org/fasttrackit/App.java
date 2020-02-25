cc7c6403e8ebaa2b7c76d8ec88262ec74cc1656f
Maier Eduard <maiereduard86@gmail.com>
2020-02-25 16:01:33 +0200

    create Task Request

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -1,16 +1,26 @@
 package org.fasttrackit;
 
+import org.fasttrackit.persistance.TaskRepository;
+import org.fasttrackit.transfer.CreateTaskRequest;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.time.LocalDate;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) throws IOException, SQLException {
 
+        CreateTaskRequest createTaskRequest = new CreateTaskRequest();
+        createTaskRequest.setDeadline(LocalDate.now().plusWeeks(1));
+        createTaskRequest.setDescription("Learn JDBC");
 
-
+        TaskRepository taskRepository = new TaskRepository();
+        taskRepository.createTask(createTaskRequest);
 
     }
 }
